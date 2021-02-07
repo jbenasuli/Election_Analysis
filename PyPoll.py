2c63341921038ccecd027bed081ac616b239c440
James Benasuli <jamiebenasuli@gmail.com>
2021-02-07 01:10:27 -0500

    adding PyPoll file

---
+++ PyPoll.py
@@ -0,0 +1,8 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of the candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on the popular vote
+
+# Import the election results csv
