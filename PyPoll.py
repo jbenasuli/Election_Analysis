fc24d3962fe03780db928b0b5353f344d0b17680
James Benasuli <jamiebenasuli@gmail.com>
2021-02-07 01:23:44 -0500

    updating PyPoll code

--- PyPoll.py
+++ PyPoll.py
@@ -6,3 +6,20 @@
 # 5. The winner of the election based on the popular vote
 
 # Import the election results csv
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+    
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the open() function with the "w" mode we will write data to the file.
+with open(file_to_save, "w") as txt_file:
+
+# Write three counties to the file.
+    txt_file.write("Counties in the Election\n------------------\nArapahoe\nDenver\nJefferson")
