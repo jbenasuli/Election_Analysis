70b4d19109b444e79af4046123e9c7ee91ad0278
James Benasuli <jamiebenasuli@gmail.com>
2021-02-07 19:23:55 -0500

    updating with candidate total vote counter

--- PyPoll.py
+++ PyPoll.py
@@ -61,29 +61,112 @@
 #     headers = next(file_reader)
 #     print(headers)
 
-##### getting the total votes #######
+# ##### getting the total votes #######
+# # Add our dependencies.
+# import csv
+# import os
+# # Assign a variable to load a file from a path.
+# file_to_load = os.path.join("Resources", "election_results.csv")
+# # Assign a variable to save the file to a path.
+# file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+# # 1. Initialize a total vote counter.
+# total_votes = 0
+
+# # Open the election results and read the file
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+
+#     # Read the header row.
+#     headers = next(file_reader)
+
+#     # Print each row in the CSV file.
+#     for row in file_reader:
+#         # 2. Add to the total vote count.
+#         total_votes += 1
+
+# # 3. Print the total votes.
+# print(total_votes)
+
+# ##### getting the candidates' names #######
+# # Add our dependencies.
+# import csv
+# import os
+# # Assign a variable to load a file from a path.
+# file_to_load = os.path.join("Resources", "election_results.csv")
+# # Assign a variable to save the file to a path.
+# file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+# # Initialize a total vote counter
+# total_votes = 0
+
+# # Declare a new list to hold candidate options
+# candidate_options = []
+
+# # Open the election results and read the file
+# with open(file_to_load) as election_data:
+#     file_reader = csv.reader(election_data)
+
+#     # Read the header row
+#     headers = next(file_reader)
+
+#     # Print each row in the CSV file
+#     for row in file_reader:
+#         # Add to the total vote count
+#         total_votes += 1
+        
+#         # Print the candidate name from each row
+#         candidate_name = row[2]
+
+#         # Use if statement to return only unique values
+#         if candidate_name not in candidate_options:
+#             # Add the candidate name to the candidate list
+#             candidate_options.append(candidate_name)
+
+# # Print the candidate list
+# print(candidate_options)
+
+##### getting the candidates' votes #######
 # Add our dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
-# 1. Initialize a total vote counter.
+# Initialize a total vote counter
 total_votes = 0
+
+# Declare a new list to hold candidate options
+candidate_options = []
+# Declare a new dictionary to hold candidates' votes
+candidate_votes = {}
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read the header row.
+    # Read the header row
     headers = next(file_reader)
 
-    # Print each row in the CSV file.
+    # Print each row in the CSV file
     for row in file_reader:
-        # 2. Add to the total vote count.
+        # Add to the total vote count
         total_votes += 1
+        
+        # Print the candidate name from each row
+        candidate_name = row[2]
 
-# 3. Print the total votes.
-print(total_votes)
+        # Use if statement to return only unique names
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+            # Initialize the candidate's total vote counter
+            candidate_votes[candidate_name] = 0
+        # Add a vote to the candidate's total count
+        candidate_votes[candidate_name] += 1
+
+
+# Print the candidate list
+print(candidate_votes)
